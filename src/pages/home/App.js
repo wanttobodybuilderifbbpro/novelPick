95554d93962188890e170611afeea48e0dcf4a04
wanttobodybuilderifbbpro <wogud114@gmail.com>
2023-10-27 16:32:45 +0900

    problem solution

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Fragment } from "react";
@@ -10,12 +10,14 @@ import Redir from "../../components/Redirect";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/question" element={<Question />} />
-        <Route path="/result" element={<Result />} />
-        <Route path="/redirect" element={<Redir />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/question" element={<Question />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/redirect" element={<Redir />} />
+        </Routes>
+      </Router>
     </>
   );
 }
